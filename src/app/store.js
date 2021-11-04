c1678bea973ecacbe8347fd218edee2ba20de366
Tanya <Leenders>
2021-11-04 12:13:43 +0100

    adding data to redux store + shown in react component

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+
+import postsReducer from "../features/posts/postsSlice";
 
 export default configureStore({
-  reducer: () => ({}),
-})
+    reducer: {
+        posts: postsReducer,
+    },
+});
+
+// we want our top-level state object to have a field named posts inside,
+// and all the data for state.posts will be updated by the postsReducer function when actions are dispatched.
