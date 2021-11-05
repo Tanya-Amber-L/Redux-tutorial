00cb03dc4e060d3be234e15ed5bc2ccff52cacaa
Tanya <Leenders>
2021-11-05 09:40:34 +0100

    adding timestamps to posts and sorting by most recent post first

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { PostAuthor } from "./postAuthor";
+import { TimeAgo } from "./timeAgo";
 
 // React components can read data from the Redux store using the useSelector hook from the React-Redux library.
 // The "selector functions" that you write will be called with the entire Redux state object as a parameter, and should return the specific data that this component needs from the store.
@@ -9,11 +10,16 @@ import { PostAuthor } from "./postAuthor";
 export const PostsList = () => {
     const posts = useSelector((state) => state.posts);
 
-    const renderedPosts = posts.map((post) => (
+    const orderedPosts = posts
+        .slice()
+        .sort((a, b) => b.date.localeCompare(a.date));
+
+    const renderedPosts = orderedPosts.map((post) => (
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
             <p className="post-content">{post.content.substring(0, 100)}</p>
             <PostAuthor userId={post.user} />
+            <TimeAgo timestamp={post.date} />
             <br />
             <Link to={`/posts/${post.id}`} className="button muted-button">
                 View Post
--- src/features/posts/singlePostPage.js
+++ src/features/posts/singlePostPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { PostAuthor } from "./postAuthor";
+import { TimeAgo } from "./timeAgo";
 
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params;
@@ -24,6 +25,7 @@ export const SinglePostPage = ({ match }) => {
                 <h2>{post.title}</h2>
                 <p className="post-content">{post.content}</p>
                 <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
                 <br />
                 <Link to={`/editPost/${post.id}`} className="button">
                     Edit Post
